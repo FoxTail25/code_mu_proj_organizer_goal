3bb2f7a09c3c45a35d5d5f0d83fb6172c434db7a
Grigoriy <foxtail25@gmail.com>
2025-02-22 10:34:28 +0300

    popup work

--- js/index.js
+++ js/index.js
@@ -2,12 +2,19 @@ import { localStorageWork } from './index/local_stor.js';
 import { userTarget } from './index/targets.js';
 import { goalPresentWork } from './index/targetVidj.js';
 
+
+// элементы PopUp
+let popUpBg = document.getElementById('popUp-bg')
+let addTargetBtn = document.getElementById('addTargetBtn');
+let popUpCloseBtn = document.getElementById('popUpCloseBtn');
+
+function openAndClosePopUp() {
+    let popUpBgOpen = [...popUpBg.classList].includes('hide')
+
+}
+
+// доступ к контейнеру целей
 let targetContainer = document.getElementById('target-container')
 
 const userGoals = [];
 
-
-goalPresentWork.addTarget()
-goalPresentWork.addTarget()
-goalPresentWork.addTarget()
-goalPresentWork.addTarget()
