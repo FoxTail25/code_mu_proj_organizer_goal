131b3637f32d1337bdfdcccc11d5db89b1076c09
Grigoriy <foxtail25@gmail.com>
2025-02-23 12:20:30 +0300

    Кажется я понял как нужно было правильно решать вчерашнюю проблемму. этот сейв рабочее но наверное не правильное решение

--- js/index/pop_up_work.js
+++ js/index/pop_up_work.js
@@ -1,6 +1,7 @@
 class PopUpWork {
   popUpBg = document.querySelector("#popUp-bg");
-  arrEventListenerElements = [];
+//   arrEventListenerElements = [];
+
 
   targetName = document.querySelector("#target-name");
   targetStartData = document.querySelector("#target-start-data");
@@ -63,6 +64,7 @@ class PopUpWork {
   addListenerOnTargetSteps() {
     console.log("сработал addListenerOnTargetSteps");
     this._stepsAddListenerToUpdateBtn();
+    this._stepsAddListenerToSpan();
     this._stepsAddListenerToDeleteBtn();
   }
   _stepsAddListenerToUpdateBtn() {
@@ -73,54 +75,61 @@ class PopUpWork {
     arrUpdateBtn.forEach((e) =>
       e.addEventListener("click", _changeSpanToInput)
     );
-  
 
-  function _changeSpanToInput(event) {
-    console.log("_changeSpanToInput");
-    let input = document.createElement("input");
-    let parentElem = event.target.parentElement;
-    let span = event.target.nextSibling;
-    let info = span.textContent;
-    input.value = info;
-    parentElem.removeChild(span);
-    event.target.insertAdjacentElement("afterend", input);
-    input.focus();
-    console.log("event.target", event.target);
-    event.target.removeEventListener("click", _changeSpanToInput);
-    event.target.removeEventListener("click", _changeSpanToInput);
-    event.target.classList.remove("add-step");
-    event.target.classList.add("save-step");
-    event.target.setAttribute("title", "сохранить");
-    event.target.addEventListener("click", _saveUpdatedStep);
+    function _changeSpanToInput(event) {
+      console.log("_changeSpanToInput");
+      let input = document.createElement("input");
+      let parentElem = event.target.parentElement;
+      let span = event.target.nextSibling;
+      let info = span.textContent;
+      input.value = info;
+      parentElem.removeChild(span);
+      event.target.insertAdjacentElement("afterend", input);
+      input.focus();
+      console.log("event.target", event.target);
+      event.target.removeEventListener("click", _changeSpanToInput);
+      event.target.removeEventListener("click", _changeSpanToInput);
+      event.target.classList.remove("add-step");
+      event.target.classList.add("save-step");
+      event.target.setAttribute("title", "сохранить");
+      event.target.addEventListener("click", _saveUpdatedStep);
+    }
+    function _saveUpdatedStep(e) {
+      console.log("_saveUpdateBtnStep");
+      let span = document.createElement("span");
+      let parentElem = e.target.parentElement;
+      let input = e.target.nextSibling;
+      let info = input.value;
+      span.textContent = info;
+      parentElem.removeChild(input);
+      e.target.insertAdjacentElement("afterend", span);
+      e.target.removeEventListener("click", _saveUpdatedStep);
+      e.target.classList.remove("save-step");
+      e.target.classList.add("add-step");
+      e.target.setAttribute("title", "редактировать");
+      e.target.addEventListener("click", _changeSpanToInput);
+    }
   }
-  function _saveUpdatedStep(e) {
-    console.log("_saveUpdateBtnStep");
-    let span = document.createElement("span");
-    let parentElem = e.target.parentElement;
-    let input = e.target.nextSibling;
-    let info = input.value;
-    span.textContent = info;
-    parentElem.removeChild(input);
-    e.target.insertAdjacentElement("afterend", span);
-    e.target.removeEventListener("click", _saveUpdatedStep);
-    e.target.classList.remove("save-step");
-    e.target.classList.add("add-step");
-    e.target.setAttribute("title", "редактировать");
-    e.target.addEventListener("click", _changeSpanToInput);
+  _stepsAddListenerToSpan() {
+    let arrSpan = [...this.stepsToTarget.children].map((e) => e.children[1]);
+    console.log(arrSpan);
+    arrSpan.forEach((e) =>
+      e.addEventListener("click", console.log('!!!!!'))
+    );
+    //   function foo(){
+    // 	console.log(this)
+    //   }
   }
-}
-_stepsAddListenerToDeleteBtn() {
+  _stepsAddListenerToDeleteBtn() {
     let arrDeleteBtn = [...this.stepsToTarget.children].map(
       (e) => e.children[2]
     );
     console.log("сработал _stepsDeleteeBtn", arrDeleteBtn);
-    arrDeleteBtn.forEach((e) =>
-      e.addEventListener("click", deleteStep)
-    );
-	function deleteStep(e) {
-			  let li = e.target.parentElement
-			  li.parentElement.removeChild(li)
-		  }
+    arrDeleteBtn.forEach((e) => e.addEventListener("click", deleteStep));
+    function deleteStep(e) {
+      let li = e.target.parentElement;
+      li.parentElement.removeChild(li);
+    }
   }
 
   // removeAllEventListener() {
@@ -161,8 +170,6 @@ _stepsAddListenerToDeleteBtn() {
 
   // 		return answer
   // };
-
-  
 
   // };
 }
